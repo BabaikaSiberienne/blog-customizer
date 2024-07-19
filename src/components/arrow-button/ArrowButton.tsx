23bb92eea58be769b4e8877d7848d9d415a08e65
BabaikaSiberienne <113935883+BabaikaSiberienne@users.noreply.github.com>
2024-07-19 23:52:58 +0300

    Revert "sedefd"

--- src/components/arrow-button/ArrowButton.tsx
+++ src/components/arrow-button/ArrowButton.tsx
@@ -1,27 +1,19 @@
 import arrow from 'src/images/arrow.svg';
-import clsx from 'clsx';
+
 import styles from './ArrowButton.module.scss';
 
 /** Функция для обработки открытия/закрытия формы */
-export type ArrowButtonProps = {
-	closeOnClick: () => void;
-	formOpened: boolean;
-};
+export type OnClick = () => void;
 
-export const ArrowButton = ({ closeOnClick, formOpened }: ArrowButtonProps) => {
+export const ArrowButton = () => {
 	return (
 		/* Не забываем указаывать role и aria-label атрибуты для интерактивных элементов */
 		<div
 			role='button'
 			aria-label='Открыть/Закрыть форму параметров статьи'
 			tabIndex={0}
-			onClick={closeOnClick}
 			className={styles.container}>
-			<img
-				src={arrow}
-				alt='иконка стрелочки'
-				className={clsx(styles.arrow, formOpened && styles.arrow_open)}
-			/>
+			<img src={arrow} alt='иконка стрелочки' className={styles.arrow} />
 		</div>
 	);
 };
